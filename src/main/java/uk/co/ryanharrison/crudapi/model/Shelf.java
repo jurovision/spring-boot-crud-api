6486965e4c494f0ed690fb88effd31b757e7e1e0
jurovision <elvofjustice@gmail.com>
2025-04-01 12:47:36 +0200

    Create Shelf class

---
+++ src/main/java/uk/co/ryanharrison/crudapi/model/Shelf.java
@@ -0,0 +1,73 @@
+package uk.co.ryanharrison.crudapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+public class Shelf {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String shelfID;
+    private String drinkName;
+    private int quantity;
+    private int price;
+    // TODO private int maxquantity?
+
+    public Shelf(String shelfID, String drinkName, int price) {
+        this.shelfID = shelfID;
+        this.drinkName = drinkName;
+        this.price = price;
+        quantity = 10;
+
+    }
+
+    public String getShelfID() {
+        return this.shelfID;
+    }
+
+    public void setShelfID(String shelfID) {
+        this.shelfID = shelfID;
+    }
+
+    public String getDrinkName() {
+        return this.drinkName;
+    }
+
+    public void setDrinkName(String drinkName) {
+        this.drinkName = drinkName;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void decreaseQuantity() {
+        this.quantity--;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+
+}
